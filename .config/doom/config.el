98925325c14ab24528320cb8bf78bbbd711728e8
UzairQamarxyz <uzairqamarxyz@gmail.com>
2023-11-07 16:24:10 +0500

    Updating doom configs
    Signed-off-by: UzairQamarxyz <uzairqamarxyz@gmail.com>

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,84 +1,17 @@
-;;; config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+;; Font
+(setq doom-font (font-spec :family "IosevkaCustom Nerd Font" :size 14)
+      doom-variable-pitch-font (font-spec :family "Proxima Nova" :size 13))
 
+;; Theme
+(setq doom-theme 'kanagawa)
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Uzair Qamar"
-      user-mail-address "uzairq@xgrid.co")
+;; Numberline
+(setq display-line-numbers-type 'relative)
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-;;
-;; (setq doom-font (font-spec :family "Iosevka Custom" :size 18)
-(setq doom-font (font-spec :family "IosevkaCustom Nerd Font" :size 18)
-      doom-variable-pitch-font (font-spec :family "Cormorant" :size 20))
-      ;; doom-variable-pitch-font (font-spec :family "Cormorant Infant" :size 20))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-tokyo-night)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type `relative)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Documents/org/")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; Org-mode
+(setq org-directory "~/org/")
 
 ;; Set Transparency
 (set-frame-parameter (selected-frame) 'alpha '(90 90))
@@ -88,19 +21,16 @@
 ;; Set fringe style
 (set-fringe-mode 0)
 
-;; Solaire Mode
-(setq solaire-global-mode +1)
-
 ;; Terraform config
 ;; (use-package lsp-mode
 ;;   :ensure t
 ;;   :hook ((terraform-mode . lsp-deferred)))
-(setq lsp-disabled-clients '(tfls))
-(setq lsp-terraform-ls-enable-show-reference t)
+;; (setq lsp-disabled-clients '(tfls))
+;; (setq lsp-terraform-ls-enable-show-reference t)
 
 ;; Ignore .terraform folders
-;; (with-eval-after-load 'lsp-mode
-;;   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.terraform\\'")
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.terraform\\'"))
 
 
 ;; Cfn-Lint
@@ -134,11 +64,4 @@ See `https://github.com/aws-cloudformation/cfn-python-lint'."
   (add-hook 'cfn-json-mode-hook 'flycheck-mode)
   (add-hook 'cfn-yaml-mode-hook 'flycheck-mode))
 
-(setq doom-gruvbox-dark-variant "hard")
-
-;; LSP UI
-(setq lsp-lens-enable t)
-(setq lsp-headerline-breadcrumb-enable t)
-(setq lsp-semantic-tokens-enable t)
-(setq lsp-semantic-tokens-honor-refresh-requests t)
-(setq lsp-enable-links t)
+(setq fancy-splash-image "/home/uzair/Pictures/vectors/wallpaper.png")
--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,8 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("2cdc179d09ce2a1cb402c3bc93514b1126c8ed7689aed6c1224a8e3c8d119fd9" default))
  '(org-agenda-files '("~/org/Notes.org"))
  '(warning-suppress-types '(((yasnippet zombie)) ((yasnippet zombie)) (defvaralias))))
 (custom-set-faces
