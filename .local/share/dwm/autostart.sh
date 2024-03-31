c3f5638585955520911cf14b5ab12618131bfbad
UzairQamarxyz <uzairqamarxyz@gmail.com>
2024-03-31 10:51:26 +0500

    Updating Iosevka config, removing symlinks, updating autostart script
    Signed-off-by: UzairQamarxyz <uzairqamarxyz@gmail.com>

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,21 +1,4 @@
-#!/bin/sh
-set -x
-
-run() {
-	[ -z "$(pidof "$*")" ] && "$@" &
-}
-run /usr/lib/xfce4/notifyd/xfce4-notifyd
-run unclutter
-run dwmblocks
-run emacs --daemon
-run xfce4-power-manager
-run /usr/lib/kdeconnectd
-run /usr/lib/mate-polkit/polkit-mate-authentication-agent-1
-run picom -cb --shadow-exclude "x = 0 && y = 0 && override_redirect = true" --focus-exclude "x = 0 && y = 0 && override_redirect = true"
-
-xset r rate 200 80 &                        # Speed xrate up
-pulseaudio --start                          # Start pulseaudio
-setxkbmap -option caps:escape               # Set Capslock to Escape
-xgamma -rgamma 0.85 -ggamma 0.85 -bgamma 1  # Set gamma
-feh --bg-fill ~/Pictures/wallpaper.png      # Set wallpaper
-numlockx on
+#! /usr/bin/env sh
+dwmblocks &
+/usr/lib/polkit-kde-authentication-agent-1 &
+feh --no-fehbg --bg-fill '/home/uzair/Pictures/wallpaper.png'
